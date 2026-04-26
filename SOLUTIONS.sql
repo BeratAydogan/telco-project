444ecfb1612d6e8fc01a6b68f82c2a64a10e634e
Berat Aydoğan <berataydogan@ogrenci.beykoz.edu.tr>
2026-04-26 14:07:24 +0300

    saved

--- SOLUTIONS.sql
+++ SOLUTIONS.sql
@@ -1,24 +1,18 @@
---1. 1. Tariff-Based Customer Queries
---1.1 List the customers who are subscribed to the 'Kobiye Destek' tariff.
---1.2 Find the newest customer who subscribed to this tariff.
-
+--1
+-- Tüm müşterileri tarifelerle beraber listeledim. tarife adı kobiye destek olanları listeledim. kayıt tarihini yükselen olarak ayarlayım en düşük olanları en üste getirip 1 tanesini seçtim.
 SELECT * FROM CUSTOMERS c
 JOIN TARIFFS t ON t.TARIFF_ID =c.TARIFF_ID 
 WHERE t.TARIFF_NAME ='Kobiye Destek'
 ORDER BY SIGNUP_DATE ASC FETCH FIRST 1 ROWS ONLY ; 
 
---2. Tariff Distribution
---2.1 Find the distribution of tariffs among the customers.
-
+--2
+--tarife ismi ve sayılarını göstermek için değerleri girdim. tarife isimlerini almak için joinle tabloları bağladım. azalana göre sıraladım. 
 SELECT TARIFF_NAME, COUNT(*)  AS TARIFF_COUNT FROM CUSTOMERS c
 JOIN TARIFFS t ON t.TARIFF_ID =c.TARIFF_ID 
 GROUP BY t.TARIFF_NAME ORDER BY TARIFF_COUNT Desc;
 
---3. Customer Signup Analysis
---3.1 Identify the earliest customers to sign up.
---(Hint: The earliest customers might not necessarily have the lowest IDs.)
---3.2 Find the distribution of these earliest customers across different cities, including the total count for each city.
-
+--3
+--ilk müşteriler olarak ilk 200 tanesini varsaydım. bunu da artan(asc) ile signup_date verisi ile çektim. daha sonra şehir ve dağılım sayısını ekrana göstermek için şehre göre gruplayıp dağılıma göre sıraya dizdim.
 SELECT CITY , Count(*) AS distribution FROM (
 	SELECT * FROM CUSTOMERS 
 	ORDER BY SIGNUP_DATE asc 
@@ -27,23 +21,17 @@ SELECT CITY , Count(*) AS distribution FROM (
 GROUP BY CITY
 ORDER BY distribution;
 
-
---4. Missing Monthly Records
---4.1 Every customer has a monthly fee, and the dataset contains this month's usage values. However, an insertion error occurred, and some customers' monthly records are missing. Identify the IDs of these missing customers.
-
---4.2 Find the distribution of these missing customers across different cities.
-
+--4
+--monthly stats tablosundaki idler ile müşteri idleri aynı. buna göre monthly_stats tablosunda olmayan müşteri idlerini çektim. ve ortaya çıkan sonucu şehirlere göre grupladım.
 SELECT CUSTOMER_ID FROM CUSTOMERS WHERE CUSTOMER_ID  NOT IN (SELECT ID FROM MONTHLY_STATS);
 
 SELECT CITY, Count(*) AS MISSING_COUNT FROM CUSTOMERS c 
-WHERE NOT EXISTS (SELECT 1 FROM MONTHLY_STATS m WHERE m.ID = c.CUSTOMER_ID  )
+WHERE NOT EXISTS (SELECT * FROM MONTHLY_STATS m WHERE m.ID = c.CUSTOMER_ID  )
 GROUP BY CITY ORDER BY MISSING_COUNT desc;
 
 
---5. Usage Analysis
---5.1 Find the customers who have used at least 75% of their data limit.
---5.2 Identify the customers who have completely exhausted all of their package limits (data, minutes, and SMS).
-
+--5
+-- yapay zekaya yaptırdım.
 SELECT  c.CUSTOMER_ID, c.CUSTOMER_NAME, m.DATA_USAGE, t.DATA_LIMIT
 FROM Customers c
 JOIN Monthly_Stats m ON c.CUSTOMER_ID = m.ID
